f8c4a33c85bd8b3fc2644868b2b2df721547e72f
Lucas Xavier <lucasxavierclaindeabreu@gmail.com>
2021-02-16 10:07:40 -0300

    first commit

---
+++ src/Components/ApiProdutos.js
@@ -0,0 +1,54 @@
+
+
+const ApiProdutos = [
+    {
+        id: Math.random(),
+        produto: 'Celular',
+        descricao: 'Motorola',
+        preco: 1200.00,
+        precoAnterior: 2000.00,
+        imagem: 'celular-motorola.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Celular',
+        descricao: 'Samsumg A23',
+        preco: 800.00,
+        precoAnterior: 5000.00,
+        imagem: 'celular-samsung.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Celular',
+        descricao: 'Iphone 11',
+        preco: 1100.00,
+        precoAnterior: 1500.00,
+        imagem: 'celular-iphone.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Geladeira',
+        descricao: 'Consul',
+        preco: 3000.00,
+        precoAnterior: 5000.00,
+        imagem: 'geladeira-consul.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Geladeira',
+        descricao: 'Electrolux',
+        preco: 2000.00,
+        precoAnterior: 4000.00,
+        imagem: 'geladeira-electrolux-2.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'TV',
+        descricao: 'LG',
+        preco: 1500.00,
+        precoAnterior: 4000.00,
+        imagem: 'lg-tv.jpg'
+    }
+
+]
+export default ApiProdutos;
---
+++ src/Components/Style.js
@@ -0,0 +1,43 @@
+
+const Style = {
+    body: {
+        backgroundColor: 'antiquewhite',
+        color: "#000",
+        fontSize: 50,
+        height: "100%"
+    },
+    bodyCard: {
+        backgroundColor: 'antiquewhite',
+        color: "#000",
+        fontSize: 50,
+        height: "100"
+    },
+    position: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    space: {
+        paddingTop: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: '800'
+    },
+    cardProducts: {
+        borderWidth: 3,
+        borderColor: "#20232a",
+        borderRadius: 6,
+        padding: 15,
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 22,
+        paddingTop: 150
+    }
+}
+
+export default Style
